41bd79d5120d620b4f9d829818280cae8834796c
saigkill <saigkill@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-11-08 14:10:47 +0000

    Added ./ before validown.sh

--- trunk/en/own2pdf.sh
+++ trunk/en/own2pdf.sh
@@ -4,6 +4,6 @@ XML=xml/OWN.xml
 FOPCONF=fop-config.xml
 XSLTFO=xslt/fo/docbook.xsl
 
-validown.sh || exit $!
+./validown.sh || exit $!
 xsltproc --xinclude --output ${XML%.xml}.fo ${XSLTFO} ${XML} && fop -c ${FOPCONF} ${XML%.xml}.fo ${XML%.xml}.pdf
 
