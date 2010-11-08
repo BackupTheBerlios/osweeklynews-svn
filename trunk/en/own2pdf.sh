b59eadf3d362d8cc128b7d35a9e7733020125eae
toms <toms@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-11-08 13:13:04 +0000

    Fix problems with validation

--- trunk/en/own2pdf.sh
+++ trunk/en/own2pdf.sh
@@ -4,6 +4,6 @@ XML=xml/OWN.xml
 FOPCONF=fop-config.xml
 XSLTFO=xslt/fo/docbook.xsl
 
-xmllint --nonet --valid --noout ${XML} || exit $!
-xsltproc --output ${XML%.xml}.fo ${XSLTFO} ${XML} && fop -c ${FOPCONF} ${XML%.xml}.fo ${XML%.xml}.pdf
+validown.sh || exit $!
+xsltproc --xinclude --output ${XML%.xml}.fo ${XSLTFO} ${XML} && fop -c ${FOPCONF} ${XML%.xml}.fo ${XML%.xml}.pdf
 
