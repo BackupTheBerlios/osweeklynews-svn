5e2476beff8cb03e194b5788d903f72f8d01a2db
toms <toms@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-11-19 08:03:10 +0000

    Used .common.sh which simplifies _a lot_

--- trunk/en/own2pdf.sh
+++ trunk/en/own2pdf.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-# The main file:
-XML=xml/OWN.xml
-# Only the filename without path components:
-BASEXML=${XML#*/}
+# Include common variables and functions
+source .common.sh
+
 # The FO output filename:
 FO=${BASEXML%.xml}.fo
 # The PDF output filename
@@ -12,26 +11,8 @@ PDF=${BASEXML%.xml}.pdf
 FOPCONF=fop-config.xml
 # The XSLT stylesheet to create FO file:
 XSLTFO=xslt/fo/docbook.xsl
-# Used XSLT processor:
-XSLT=xsltproc
 
-# validown.sh creates .OWN.xml file, could be used in following steps
-./validown.sh || exit $!
-echo "Validation successful. Creating PDF..."
-echo "Using $XSLT..."
-
-if [ "$XSLT" = 'saxon' ]; then
- # The default saxon script supports catalogs, so there is no need to
- # add the -x, -y, and -t options. Default configuration file for XML
- # catalogs is /etc/java/resolver/CatalogManager.properties
- # Uncomment the following line, if you want some debugging messages:
- # export VERBOSE=1
- saxon -o ${FO} xml/.${BASEXML} ${XSLTFO}
-elif [ "$XSLT" = 'xsltproc' ]; then
- xsltproc --xinclude --output ${FO} ${XSLTFO} ${XML} && fop -c ${FOPCONF} ${FO} ${PDF}
-# elif [ "$XSLT" = 'xalan' ]; then
-# ...
-else
- echo "ERROR: Unsupported XSLT processor $XSLT"
-fi
+validate
+transform xml/.${BASEXML} ${XSLTFO} ${FO}
+fop -c ${FOPCONF} ${FO} ${PDF}
 
