5ff3c35fa9ce75bba98a0c029d39faf5dc78e0b1
toms <toms@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-11-26 08:50:27 +0000

    Output directory is now pdf/; added link to graphics

--- trunk/en/own2pdf.sh
+++ trunk/en/own2pdf.sh
@@ -3,16 +3,24 @@
 # Include common variables and functions
 source ./.common.sh
 
+# The PDF output directory:
+PDFDIR=pdf/
 # The FO output filename:
-FO=${BASEXML%.xml}.fo
+FO=${PDFDIR}${BASEXML%.xml}.fo
 # The PDF output filename
-PDF=${BASEXML%.xml}.pdf
+PDF=${PDFDIR}${BASEXML%.xml}.pdf
 # The FOP configuration filename:
 FOPCONF=fop-config.xml
 # The XSLT stylesheet to create FO file:
 XSLTFO=xslt/fo/docbook.xsl
 
 validate
+[ -d ${PDFDIR} ] || mkdir -p ${PDFDIR}
+# HACK: This will change:
+pushd ${PDFDIR}
+ln -sf ../xslt/common .
+popd
+
 transform xml/.${BASEXML} ${XSLTFO} ${FO}
 fop -c ${FOPCONF} ${FO} ${PDF}
 
