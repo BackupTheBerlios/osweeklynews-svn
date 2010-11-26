a4ac5bcd1fa6a40c97248d74744240b827c41f9e
toms <toms@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-11-26 08:29:43 +0000

    Added HTMLDIR variable and copied all CSS files to it

--- trunk/en/own2xhtml-single.sh
+++ trunk/en/own2xhtml-single.sh
@@ -3,10 +3,13 @@
 # Include common variables and functions
 source ./.common.sh
 
-# The FO output filename:
-HTML=${BASEXML%.xml}.html
+# The HTML output directory:
+HTMLDIR=xhtml/
+# The HTML output filename:
+HTML=${HTMLDIR}${BASEXML%.xml}.html
 # The XSLT stylesheet to create single HTML file:
 XSLTHTML=xslt/xhtml/docbook.xsl
 
 validate
 transform xml/.${BASEXML} ${XSLTHTML} ${HTML}
+cp xslt/xhtml/*.css ${HTMLDIR}
