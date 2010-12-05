c8602f2bd7906978405be02c4530befc9de41c7e
toms <toms@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-12-05 17:49:46 +0000

    Improved script so it can be universally used by other scripts: Parse options with getopt, possible options are --xml, --xslt, and --output

--- trunk/en/own2xhtml-single.sh
+++ trunk/en/own2xhtml-single.sh
@@ -3,20 +3,52 @@
 # Include common variables and functions
 source ./.common.sh
 
-# The HTML output directory:
-HTMLDIR=xhtml/
-# The HTML output filename:
-HTML=${HTMLDIR}${BASEXML%.xml}.html
-HTMLNEU=${HTMLDIR}/OWNNEU.html
-# The XSLT stylesheet to create single HTML file:
-XSLTHTML=${XSLTDIR}/xhtml/docbook.xsl
+# Our default values
+_XML="xml/.${BASEXML}"
+_XSLT="${XSLTHTML}"
+_OUT="${HTML}"
+
+
+#export LC_ALL=C
+TEMP=$(getopt -o h -l "help,xml:,xslt:,output:" -n "$0" -- "$@")
+eval set -- "$TEMP"
+
+while true
+do
+   case $1 in
+    -h|--help)
+      printf "${0#*/} [--xml | --xslt | --output ]\n\n"
+      printf "Transform an OWN XML file to a single XHTML.\n"
+      exit 0
+      ;;
+    --xml)
+      [ -a "$2" ] || error "XML file '$2' not found."; exit 10
+      _XML=$2
+      shift
+      ;;
+    --xslt)
+      [ -a "$2" ] || error "XSLT stylesheet '$2' not found."; exit 20
+      _XSLT=$2
+      shift
+      ;;
+    --output)
+      _OUT=$2
+      shift
+      ;;
+    --) shift ; break ;;
+    *)
+      printf "Unknown option $1\n"
+      exit 100
+      ;;
+   esac
+   shift
+done
+
+debug "XML=$_XML, XSLT=$_XSLT, OUT=$_OUT"
 
 validate
-transform xml/.${BASEXML} ${XSLTHTML} -o "${HTML}"
-echo "Stripping the Pathes for pictures"
-sed 's/common\/logos/http:\/\/news.opensuse.org\/wp-content\/uploads\/2010\/12/g' ${HTML} > ${HTMLNEU} 
-echo "done ..."
-#
+transform ${_XML} ${_XSLT} -o "${_OUT}" $@
+
 cp xslt/xhtml/*.css ${HTMLDIR}
 (cd ${HTMLDIR}
 ln -sf ${XSLTDIR}/common .
