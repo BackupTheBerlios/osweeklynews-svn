6d68a015d34b64fb9895345e65f53c05d9eb7821
saigkill <saigkill@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-12-05 18:44:31 +0000

    Added "source" and "./"

--- trunk/en/own2wp-xhtml.sh
+++ trunk/en/own2wp-xhtml.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+source ./.common.sh
 
 # Where to save the Wordpress file:
 WP=${HTMLDIR}/${BASEXML%.xml}-wp.html
 
-own2xhtml-single.sh --output ${WP} \
+./own2xhtml-single.sh --output ${WP} \
   logos.src.path=news.opensuse.org/wp-content/uploads/2010/12/ $@
 
 
