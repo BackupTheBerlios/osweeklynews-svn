826734848ddebeaf9daa43b4487f4c72c5a7668f
saigkill <saigkill@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-12-05 14:44:46 +0000

    Published the HTML Version of the Weekly News. Added 2 Pictures because same Name using in HTML and PDF. Added a sed Routine to own2xhtml.

--- trunk/en/own2xhtml-single.sh
+++ trunk/en/own2xhtml-single.sh
@@ -7,11 +7,15 @@ source ./.common.sh
 HTMLDIR=xhtml/
 # The HTML output filename:
 HTML=${HTMLDIR}${BASEXML%.xml}.html
+HTMLNEU=${HTMLDIR}/OWNNEU.html
 # The XSLT stylesheet to create single HTML file:
 XSLTHTML=${XSLTDIR}/xhtml/docbook.xsl
 
 validate
 transform xml/.${BASEXML} ${XSLTHTML} -o "${HTML}"
+echo "Stripping the Pathes for pictures"
+sed 's/common\/logos/http:\/\/news.opensuse.org\/wp-content\/uploads\/2010\/12/g' ${HTML} > ${HTMLNEU} 
+echo "done ..."
 #
 cp xslt/xhtml/*.css ${HTMLDIR}
 (cd ${HTMLDIR}
