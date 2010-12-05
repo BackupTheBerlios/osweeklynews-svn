2a3cc4d2a1bef58d0add134d5973d2d1faf78b0f
toms <toms@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-12-05 11:49:40 +0000

    - Added DEBUG variable and debug() output function - Separated transform() function into transform_xsltproc() and   transform_saxon(); slightly changed output file with additional   -o option to distinguish from the PARAM=VALUE options - Simplified transform() function and called transform_*()

--- trunk/en/.common.sh
+++ trunk/en/.common.sh
@@ -11,7 +11,8 @@ BASEXML=${XML#*/}
 XSLT=${XSLT:-"xsltproc"}
 # The default absolute XSLT stylesheet directory
 XSLTDIR=$PWD/xslt/
-
+# Default Debugging value is no, if nothing is set:
+DEBUG=${DEBUG:-no}
 
 
 ## Outputs error messages
@@ -19,10 +20,14 @@ error() {
   echo -e "\033[31m[ERROR] $1\033[m\017"
 }
 
-
 ## Outputs information messages
 info() {
-  echo -e "\033[34m[Info] $1\033[m\017"
+  echo -e "\033[34m[Info ] $1\033[m\017"
+}
+
+## Outputs debugging information when variable DEBUG=yes
+debug() {
+  [ "$DEBUG" = 'yes' ] && echo -e "\033[33m[Debug] $1\033[m\017"
 }
 
 
@@ -33,36 +38,79 @@ validate() {
  info "Validation successful."
 }
 
+## Transforms XML file with xsltproc
+function transform_xsltproc()
+{
+  # Parameters:
+  # $1 = XML input file
+  # $2 = XSLT stylesheet
+  # $3 = optional output file, needed -o FILE
+  # $4 = option in the format PARAMETER=VALUE
+  # $5 ... like $4
+  xmlfile=$1
+  xsltfile=$2
+  shift 2
+  output=""
+  options=""
+
+  if [ "$1" = "-o" -o "$1" = '--output' ]; then
+     output="-o $2"
+     shift 2
+  fi
+  # Iterate through all options and split them into
+  # parameter and value
+  while true
+  do
+     if [ "$1" = '' ]; then
+        break
+     fi
+     param=${1%=*}
+     value=${1#*=}
+     options="$options --stringparam $param $value"
+     shift
+  done
+
+  debug "xsltproc --xinclude $output $options $xsltfile $xmlfile"
+  xsltproc --xinclude $output $options $xsltfile $xmlfile
+}
+
+## Transforms XML file with Saxon
+function transform_saxon()
+{
+  # Parameters:
+  # $1 = XML input file
+  # $2 = XSLT stylesheet
+  # $3 = optional output file, needed -o FILE
+  # $4 = option in the format PARAMETER=VALUE
+  # $5 ... like $4
+  xmlfile=$1
+  xsltfile=$2
+  shift 2
+  output=""
+
+  if [ "$1" = "-o" -o "$1" = '--output' ]; then
+     output="-o $2"
+     shift 2
+  fi
+
+  options=$@
+  debug "saxon $output $xmlfile $xsltfile  $options"
+  saxon $output $xmlfile $xsltfile  $options
+}
 
 ## Transforms XML file with XSLT stylesheet into an output format
 transform() {
   # Parameters:
   # $1 = XML input file
   # $2 = XSLT stylesheet
-  # $3 = optional output file
-  if [ "$XSLT" = 'saxon' ]; then
- # The default saxon script supports catalogs, so there is no need to
- # add the -x, -y, and -t options. Default configuration file for XML
- # catalogs is /etc/java/resolver/CatalogManager.properties
- #
- # Uncomment the following line, if you want some debugging messages:
- # export VERBOSE=1
- OUTPUT=""
- if [ "$3" != '' ]; then
-   OUTPUT="-o $3"
- fi
- saxon $OUTPUT $1 $2
-elif [ "$XSLT" = 'xsltproc' ]; then
- OUTPUT=""
- if [ "$3" != '' ]; then
-   OUTPUT="--output $3"
- fi
- xsltproc --xinclude $OUTPUT $2 $1
+  # $3 = optional output file, needed -o FILE
+  # $4 = options
 
-# We do not support Xalan at the moment, but only time will tell...
-# elif [ "$XSLT" = 'xalan' ]; then
-# ...
-else
- error "ERROR: Unsupported XSLT processor $XSLT"
-fi
+  if [ "$XSLT" = 'saxon'  ]; then
+    transform_saxon $*
+  elif [ "$XSLT" = 'xsltproc' ]; then
+    transform_xsltproc $*
+  else
+    error "ERROR: Unsupported XSLT processor $XSLT"
+  fi
 }
