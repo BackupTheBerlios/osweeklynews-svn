ec3c63dc02b42811fa8ecf4c948ba4195e0b9009
toms <toms@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-12-05 17:50:25 +0000

    New script for Wordpress output, especially useful as it changes the logos.src.path

---
+++ trunk/en/own2wp-xhtml.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Where to save the Wordpress file:
+WP=${HTMLDIR}/${BASEXML%.xml}-wp.html
+
+own2xhtml-single.sh --output ${WP} \
+  logos.src.path=news.opensuse.org/wp-content/uploads/2010/12/ $@
+
+
