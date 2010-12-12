f929e3c0d68abf72cf8b1c01d3fcb69a1036ea56
saigkill <saigkill@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2010-12-12 04:07:15 +0000

    Added Readytable Link to the Issue.

--- trunk/en/own2wp-xhtml.sh
+++ trunk/en/own2wp-xhtml.sh
@@ -16,6 +16,6 @@ source ./.common.sh
 WP=${HTMLDIR}/${BASEXML%.xml}-wp.html
 
 ./own2xhtml-single.sh --output ${WP} \
-  logos.src.path=news.opensuse.org/wp-content/uploads/2010/12/ $@
+  logos.src.path=http://news.opensuse.org/wp-content/uploads/2010/12/ $@
 
 
