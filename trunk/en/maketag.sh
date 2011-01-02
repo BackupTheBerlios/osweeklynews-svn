e31936468a7131b05f80908bc450911f21bd4922
toms <toms@8e4e73ea-523c-4fc9-a60d-32745654d82e>
2011-01-02 14:07:05 +0000

    - Added sanity check: only proceed if user enters a tag - Only output success when svn copy was successful too

--- trunk/en/maketag.sh
+++ trunk/en/maketag.sh
@@ -3,10 +3,16 @@
 # As example if you want to make a tag to Issue 152 you have to start this Script like:
 # $ ./maketag.sh 152
 ISSUE=$1
+
+# Check for empty issue:
+if [ "$ISSUE" = "" ]; then
+  echo "ERROR: Need an issue number for tagging."
+  exit 10
+fi
+
 echo "Create a tag for Issue $ISSUE ..."
 
 svn copy http://svn.berlios.de/svnroot/repos/osweeklynews/trunk/en \
-   http://svn.berlios.de/svnroot/repos/osweeklynews/tags/ownen$ISSUE
-
-echo "Tag correct set."
+   http://svn.berlios.de/svnroot/repos/osweeklynews/tags/ownen$ISSUE \
+&& echo "Tag correct set."
 
